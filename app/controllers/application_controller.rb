e1d5e4c8c0c95d0b3f3b1410abb099decbe3bc06
AbdelrahmanMohamed <abdelrahman.muhamedd@gmail.com>
2022-03-05 03:41:00 +0200

    Switching between language Button Added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :configure_permitted_parameters, if: :devise_controller?
-  around_action :switch_locale
+    around_action :switch_locale
 
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
-  end 
+    def switch_locale(&action)
+      locale = params[:locale] || I18n.default_locale
+      I18n.with_locale(locale, &action)
+    end 
 
-  
   protected
 
   def configure_permitted_parameters
